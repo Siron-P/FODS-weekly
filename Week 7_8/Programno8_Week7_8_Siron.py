c6e2d825651795bc986b1f520712993b142d5ff3
Siron-P <psiron24@tbc.edu.np>
2025-04-14 20:10:21 +0545

    Add files via upload

---
+++ Week 7_8/Programno8_Week7_8_Siron.py
@@ -0,0 +1,31 @@
+import pandas as pd\
+
+#Create two Pandas Series
+series1 = pd.Series([10, 20, 30, 40, 50])
+series2 = pd.Series([1, 2, 3, 4, 5])
+
+#Add the two Series
+addition = series1 + series2
+
+#Subtract he two Series
+subtraction = series1 - series2
+
+#multiply the two Series
+multiplication = series1 + series2
+
+# Divide the two Series
+division = seriesi / series2
+
+#Print the results
+print("Series 1:")
+print(series1)
+print("Series 2:")
+print(series2)
+print("Addition:")
+print(addition)
+print("Subtraction:")
+print(subtraction)
+print("Multiplication:")
+print(multiplication)
+print("Division:")
+print(division)
